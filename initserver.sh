6a446f43cbfad499092263206263c227607bf65b
gunslinger <1007708137@qq.com>
2020-04-23 01:44:52 +0800

    Update initserver.sh

--- initserver.sh
+++ initserver.sh
@@ -3,8 +3,9 @@
 #修复权限
 chown -R steam:steam $STEAMAPPDIR
 chmod -R 755 $STEAMAPPDIR
-su steam -c "startserver.sh"
 
 #叛乱修复
 sed -i 's@/steam.sh/@/steamcmd.sh/@g' $STEAMAPPDIR/srcds_run
 rm -f $STEAMAPPDIR/bin/libstdc++.so.6
+
+su steam -c "startserver.sh"
